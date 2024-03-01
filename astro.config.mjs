7f6e2827b2de1c1c9d3d6dc35abe5c38f8c2fa3a
Jake C <jakeychap@gmail.com>
2024-03-01 21:54:40 +0000

    improving seo and fixing vulnerabilites

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
-import vercelStatic from '@astrojs/vercel/static';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import vercelStatic from "@astrojs/vercel/static";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react()],
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    sitemap(),
+  ],
   site: `https://www.sybline.com`,
-  output: 'static',
+  output: "static",
   adapter: vercelStatic({
     analytics: true,
   }),
